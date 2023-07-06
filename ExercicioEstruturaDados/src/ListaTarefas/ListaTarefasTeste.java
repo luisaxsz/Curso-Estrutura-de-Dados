42c0d73fe8cb2ce3505458b8c8fb6d7ac89b5340
Maria Luisa Lameirão Sousa <marialuisalameirao@gmail.com>
2023-07-06 18:47:06 -0300

    Update no exercício de lista de tarefas

--- ExercicioEstruturaDados/src/ListaTarefas/ListaTarefasTeste.java
+++ ExercicioEstruturaDados/src/ListaTarefas/ListaTarefasTeste.java
@@ -7,8 +7,9 @@ public class ListaTarefasTeste {
 	public static void main(String[] args) {
 		ArrayList<ListaTarefas> listaTarefas = new ArrayList<ListaTarefas>(5);
 		adicionarTarefa(5, listaTarefas);
-		
-		System.out.println(listaTarefas.toString());
+		mostrarLista(listaTarefas);
+		removerTarefa(0,listaTarefas);
+		mostrarLista(listaTarefas);
 	}
 
 	public static void adicionarTarefa(int quantidade, ArrayList<ListaTarefas> listaTarefas) {
@@ -18,6 +19,26 @@ public class ListaTarefasTeste {
 			System.out.println("Insira a tarefa: ");
 			tarefas.setTarefas(scan.nextLine());
 			listaTarefas.add(tarefas);
+		}
+	}
+	
+	public static void removerTarefa(int posicao, ArrayList<ListaTarefas> listaTarefas){
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Digite numero da tarefa que quer remover: ");
+		posicao = scan.nextInt();
+		if(posicao == 1) {
+			listaTarefas.remove(0);
+		}else {
+			listaTarefas.remove(posicao-1);
+		}
+	}
+	
+	public static void mostrarLista(ArrayList<ListaTarefas> listaTarefas) {
+		int tamanho = 1;
+		for (ListaTarefas tarefas : listaTarefas) {
+			System.out.print(tamanho + " - ");
+			System.out.println(tarefas);
+			tamanho++;
 		}
 	}
 }
