6f8dfcde389838c268ec6814a2b8f7a1833e8982
Maria Luisa Lameirão Sousa <marialuisalameirao@gmail.com>
2023-07-07 11:12:36 -0300

    Criação de menu para navegação

--- ExercicioEstruturaDados/src/ListaTarefas/ListaTarefasTeste.java
+++ ExercicioEstruturaDados/src/ListaTarefas/ListaTarefasTeste.java
@@ -7,11 +7,32 @@ public class ListaTarefasTeste {
 	public static void main(String[] args) {
 		ArrayList<ListaTarefas> listaTarefas = new ArrayList<ListaTarefas>();
 		
-		adicionarTarefa(5, listaTarefas);
-		mostrarLista(listaTarefas);
-		removerTarefa(0,listaTarefas);
-		mostrarLista(listaTarefas);
-		limparLista(listaTarefas);
+		int opcao = 1;
+		
+		while (opcao != 0) {
+			opcao = menu(0);
+			
+			switch(opcao) {
+			case 0: System.out.println("Lista fechada!");break;
+			case 1: adicionarTarefa(5, listaTarefas); break;
+			case 2: removerTarefa(5, listaTarefas); break;
+			case 3: mostrarLista(listaTarefas); break;
+			case 4: limparLista(listaTarefas); break;
+			default: System.out.println("Insira um valor valido!");break;
+			}
+			
+		}
+	}
+	
+	public static int menu(int opcao) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("[1] Adicionar Tarefas");
+		System.out.println("[2] Remover Tarefa");
+		System.out.println("[3] Mostrar Lista");
+		System.out.println("[4] Limpar Lista");
+		System.out.println("[0] Sair");
+		opcao = scan.nextInt();	
+		return opcao;
 	}
 
 	public static void adicionarTarefa(int quantidade, ArrayList<ListaTarefas> listaTarefas) {
