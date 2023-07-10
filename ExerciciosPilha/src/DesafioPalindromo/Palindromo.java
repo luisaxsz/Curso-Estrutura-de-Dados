50d70edaeb978dd2d998392b11f3a55e8cab941e
Maria Luisa Lameirão Sousa <marialuisalameirao@gmail.com>
2023-07-10 12:49:03 -0300

    update desafio palindromo

--- ExerciciosPilha/src/DesafioPalindromo/Palindromo.java
+++ ExerciciosPilha/src/DesafioPalindromo/Palindromo.java
@@ -12,22 +12,26 @@ public class Palindromo {
 	}
 	
 	public static boolean verificaPalindromo(String palavra) {
+		//cria a pilha
 		Stack<Character> pilha = new Stack <Character>();
 		Scanner scan = new Scanner(System.in);
 		
+		//entrada da palavra
 		System.out.println("Teste a palavra: ");
 		palavra = scan.nextLine();
 		
+		//empilha os caracteres da palavra
 		for (int i  = 0; i < palavra.length(); i++) {
 			pilha.add(palavra.charAt(i));
 		}
 		
 		String palavraInversa = "";
 		
+		//inverte a palavra
 		while (!pilha.isEmpty()) {
 			palavraInversa += pilha.pop();
 		}
-		
+		//verifica se palavras sao iguais
 		if(palavra.equalsIgnoreCase(palavraInversa)) {
 			return true;
 		}
